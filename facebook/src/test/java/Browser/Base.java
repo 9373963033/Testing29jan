438b6655a6efdd916be8e9922261b2e1b84fe313
VIKRAM PAWAR <VIKRAM PAWAR@LAPTOP-BBM6834N>
2022-06-06 01:23:29 +0530

    add the all class

---
+++ facebook/src/test/java/Browser/Base.java
@@ -0,0 +1,44 @@
+package Browser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class Base {
+
+	
+	
+	public static WebDriver openChromeBrowser() {
+		 System.setProperty("webdriver.chrome.driver","D:\\selenum\\chromedriver_win32 (3)\\chromedriver.exe");
+		 WebDriver driver=new ChromeDriver();
+		 return driver;
+
+	}
+	
+//	public static WebDriver OpenFireFoxBrowser() {
+//		System.setProperty("webdriver.gecko.driver","D:\\selenum\\geckodriver.exe" );
+//		WebDriver driver= new FirefoxDriver();
+//		return driver; 
+//	}
+	
+	public static WebDriver EdgeBrowser() {
+		
+		System.setProperty("webdriver.edge.driver","D:\\firefoxb\\msedgedriver.exe" );
+		WebDriver driver= new  EdgeDriver();
+
+		return driver;
+		
+	}
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+}
